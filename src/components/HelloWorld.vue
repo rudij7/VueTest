f4782567deeb428f2934041fdfe08201473e9ef6
Rutash Joshipura <joshipurarutash@gmail.com>
2020-07-30 02:41:03 +0530

    Fix edit and delete buttons and modify json files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,44 @@
 <template>
   <v-container>
     <v-simple-table>
-    
       <tbody>
-        <tr v-for= "(item, index) in data" :key="item.id">
-          <td>{{item.id}}</td>
-          <td><v-text-field :value=item.value flat :disabled="disabled"></v-text-field></td>
-          <v-btn v-on:click='disabled = !disabled'>Edit</v-btn>
-          <v-btn v-on:click="remove(index)">Delete</v-btn>
+        <tr v-for="(item, index) in data" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>
+            <v-text-field
+              :value="item.value"
+              flat
+              :disabled="!(disabled == index)"
+            ></v-text-field>
+          </td>
+          <v-btn v-if="disabled != index" v-on:click="edit(index)">Edit</v-btn>
+          <v-btn v-if="disabled == index" v-on:click="edit(index)">Save</v-btn>
+          &nbsp;&nbsp;&nbsp;<v-btn v-on:click="remove(index)">Delete</v-btn>
         </tr>
-        
       </tbody>
-      
-    </v-simple-table> 
+    </v-simple-table>
   </v-container>
 </template>
 
 <script>
-import items from './json/items.json'
+import items from "./json/tableData7Records.json";
 
-  export default {
-    name: 'HelloWorld',
-    
-    data: () => ({
-      disabled: true,
-      methods: {
-        remove: function(index) {
-          this.data.splice(index, 1);
+export default {
+  name: "HelloWorld",
+  methods: {
+    remove: function(index) {
+      this.data.splice(index, 1);
+    },
 
-        }
-      },
-        data: items         
-    }),
-  }
+    edit: function(index) {
+      if (this.disabled == -1) {
+        this.disabled = index;
+      } else if (this.disabled == index) this.disabled = -1;
+    },
+  },
+  data: () => ({
+    disabled: -1,
+    data: items,
+  }),
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+import VueVirtualScroller from "vue-virtual-scroller";
+
+Vue.use(VueVirtualScroller);
 
 Vue.config.productionTip = false
 
